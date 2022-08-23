cc08260f00440adf6a474685c86faf2263a39dbe
yeoleummoon <yoruminn@naver.com>
2022-08-24 01:10:49 +0900

    project set up & add firebase

---
+++ src/service/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/database';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
